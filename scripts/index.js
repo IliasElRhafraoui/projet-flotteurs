65799c254f86cc23ba90dd49b4ef18f68df3893a
IliasElRahfraoui <ilias29490@gmail.com>
2024-06-26 16:53:57 +0200

    traveaux carte

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,10 @@ L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/M
     attribution: '&copy; esri'
 }).addTo(map);
 
-setTimeout(() =>  map.invalidateSize(), 1500);
+setTimeout(() => map.invalidateSize(), 1500);
+
+//liste des points
+let listPoints = [];
 
 let floatListStatic = [
     {
@@ -44,7 +47,7 @@ let floatListStatic = [
 ];
 
 
-fetch('https://fleetmonitoring.euro-argo.eu/floats/multi-lines-search/pages?page=1&size=6', {
+fetch('https://fleetmonitoring.euro-argo.eu/floats/multi-lines-search/pages?page=1&size=75', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
@@ -62,7 +65,6 @@ fetch('https://fleetmonitoring.euro-argo.eu/floats/multi-lines-search/pages?page
     return response.json()
 }).then(function (floatListDynamic) {
     floatListDynamic.forEach(function (float) {
-        console.log(float)
         let card = templateHtml.replace(/{{wmo}}/g, float.wmo);
         card = card.replace(/{{statusCode}}/g, float.statusCode);
         card = card.replace(/{{lastCycleBasicInfo.date}}/g, float.lastCycleBasicInfo.date);
@@ -73,20 +75,37 @@ fetch('https://fleetmonitoring.euro-argo.eu/floats/multi-lines-search/pages?page
         main.innerHTML = main.innerHTML + card;
 
         //Dessiner le point flotteur sur la carte 
+        if (float.lastCycleBasicInfo.lat != null && float.lastCycleBasicInfo.lon != null) {
+            let = marker = L.circleMarker([float.lastCycleBasicInfo.lat, float.lastCycleBasicInfo.lon], {
+                radius: 5,
+                color: '#ffc107',
+                fill: true,
+                stroke: false,
+                fillOpacity: 1
+            }).addTo(map)
+                .bindPopup(`<strong>WMO: </strong> ${float.wmo} <br>
+                        <strong>Type de platforme: </strong> ${float.platform.type} <br><br>
+                        <a class="voir-plus-btn btn btn-primary" role="button" href="#${float.wmo}">Voir plus d'infos</a>`);
+        
+        marker.wmo = float.wmo;
+        listPoints.push(marker);
+        }
 
-       // L.marker([float.lastCycleBasicInfo, -16.09]).addTo(map)
     });
+
 });
 
 let templateHtml = `
-<div  class="card">
-    <div id="myImage">
-        <button onclick="afficherNumCycle({{wmo}},{{lastCycleBasicInfo.numCycle}})">Voir le nombre de cicles</button>
-        <span id="numCycle{{wmo}}"></span>
-    </div>
+<div id="{{wmo}}" class="card">
+  
     <div class="card-header">
         <span class="status">
             <div class=" {{statusCode}}-circle"></div>
+            <button class="btn-map btn " onclick="afficherFloatDansLaCarte({{wmo}})">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
+                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484q-.121.12-.242.234c-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
+                </svg>
+            </button>
         </span>
         <span class="wmo">
             <strong>{{wmo}}</strong>
@@ -95,16 +114,22 @@ let templateHtml = `
 
 
 
-    <div class="card-body">
-        <div class="img-container">
+    <div class="card-body-float">
+        <div  class="img-container">
             <img class="float" src="images/flotteur.png">
         </div>
         <div class="content">
 
-            <b>Date</b>: {{lastCycleBasicInfo.date}}<br>
-            <b>Type de platforme</b>: {{plarform.type}}<br>
-            <b>System de transmission</b>: {{transmissionSystem}}<br>
-            <b>Status de batterie</b>: {{battery.status}} <br>
+            <div class="content-text">
+                <b>Date</b>: {{lastCycleBasicInfo.date}}<br>
+                <b>Type de platforme</b>: {{plarform.type}}<br>
+                <b>System de transmission</b>: {{transmissionSystem}}<br>
+                <b>Status de batterie</b>: {{battery.status}} <br>
+            </div>
+            <div id="cycle-line">
+                <button class="btn-cycle btn btn-warning" onclick="afficherNumCycle({{wmo}},{{lastCycleBasicInfo.numCycle}})">Voir le dernier cycle</button>
+                <strong class="num-cycle" id="numCycle{{wmo}}"></strong>
+            </div>
         </div>
     </div>
 </div>`;
@@ -121,6 +146,19 @@ function afficherNumCycle(wmo, numeroCycle) {
     }
     else {
         numCycle.innerHTML = "";
+    }
+}
+
+function afficherFloatDansLaCarte(wmo){
+    let exist = false;
+    listPoints.forEach(function(marker){
+        if (marker.wmo == wmo){
+            marker.openPopup();
+            exist = true;
+        }
+    });
+    if(exist==false){
+        alert("Ce flotteur n'a pas encore de coordonnés :(")
     }
 }
 
